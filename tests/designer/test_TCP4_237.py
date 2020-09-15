3698bbdb311667482a4fa12d8478733359eb7c5c
karlOS123testing <carlos.camacho@processmaker.com>
2020-09-15 16:51:36 -0400

    comment the log.append

--- tests/designer/test_TCP4_237.py
+++ tests/designer/test_TCP4_237.py
@@ -18,7 +18,7 @@ import unittest
 class TCP4_237(BaseTest):
     ''' Test to verify the creation of users with special characters '''
 
-    def test_create_active_category(self):
+    def test_create_active_category(%self):
         ''' Create an active screen category '''
         #Constants
         category_data = {}       # To save user data, when you create a category
@@ -29,17 +29,17 @@ class TCP4_237(BaseTest):
         PageProcess = PageProcesses(self.driver, data)
 
         # STEP 1: Load login page.
-        self.log.append('Step 1: Load Login page')
+        #self.log.append('Step 1: Load Login page')
         self.driver.get(data['server_url'])
         login_page = PageLogin(self.driver, data)
         login_page.login()
 
         # STEP 2: Go to Designer.
-        self.log.append('Step 2: Go to Designer')
+        #self.log.append('Step 2: Go to Designer')
         pageMenu.goto_designer()
 
         # STEP 3: Create a new Category with active status.
-        self.log.append('STEP 3: Create a new category with active status.')
+        #self.log.append('STEP 3: Create a new category with active status.')
         category_data = PageProcess.create_category('active')
         
         try:
