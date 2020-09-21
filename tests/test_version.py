7a53450e3799a5a602ad466e19d272fc9b9ab174
Benjamin Fields <benjamin.fields@processmaker.com>
2020-09-21 14:08:10 -0700

    Modified the for loop. Updated the loop to match what the newer Ellucian version check. Also added some regex stuff to make the correct/incorrect log stuff a bit nicer.

--- tests/test_version.py
+++ tests/test_version.py
@@ -49,12 +49,11 @@ class TestServerVersions(BaseTest):
         ''' Verify server & package version numbers '''
         self.log.append('Open "About" Page')
         self.wait.until(EC.visibility_of_element_located((By.ID, 'userMenu')))
-        self.log.append('Pull page source')
+        self.log.append('Pull page source\n')
         page_source = self.driver.page_source
 
-        self.log.append('Check PM4 version: ')
-#        pm4 = read_from_json_file('/Users/bfields/dev/pm4-selenium-tests', '/includes/expected_values.json', 'PM4')
-        pm4 = read_from_json_file(self.driver.data['repository_path'], '/includes/expected_values.json', 'PM4')
+        self.log[-1] += 'Check PM4 version: '
+        pm4 = read_from_json_file(data['repository_path'], '/includes/expected_values.json', 'PM4')
         server_version = re.search(r'(?<=ProcessMaker 4 v)([\d].+)(?:<\/div>)', page_source).group(1)
 
         try:
@@ -62,43 +61,35 @@ class TestServerVersions(BaseTest):
             self.assertTrue(pm4['Version'] in server_version)
             # Add success message to log if assertion succeeded
             self.log[-1] += 'Correct: ' + server_version
-            #self.log.append('Correct: ' + server_version)
 
-#        except AssertionError as e:
-        except:
+        except AssertionError as e:
             # Add failure message to log if assertion failed
-            self.log[-1] += 'Invalid! Expecting: ' + pm4['Version'] + ' - saw: ' + server_version
-            #self.log.append('Invalid! Expecting: ' + pm4['Version'] + ' - saw: ' + server_version)
-
-#            self.assertionFailures.append(str(e))
-            #self.fail()
-            fail_flag = 1
+            self.log[-1] += 'Invalid Server Version! Expecting: ' + pm4['Version'] + ' - saw: ' + server_version + '; '
+            self.assertionFailures.append(str(e))
 
         # Retrieve Custom Plugins dictionary from expected_values.json
-#        pm4_packages = read_from_json_file('/Users/bfields/dev/pm4-selenium-tests', '/includes/expected_values.json', 'Custom Packages')
-        pm4_packages = read_from_json_file(self.driver.data['repository_path'], '/includes/expected_values.json', 'Custom Packages')
+        pm4_packages = read_from_json_file(data['repository_path'], '/includes/expected_values.json', 'Custom Packages')
 
         # Verify all packages are visible on page with correct version
         packages = [element.text for element in self.driver.find_elements_by_class_name('list-group-item')]
-        fail_flag = 0
-        self.log.append('Package Versions: ')
+
+        self.log[-1] += 'Package Versions: '
         for elem in packages:
             for key in pm4_packages.keys():
                 if key in elem:
                     try:
                         self.assertTrue(pm4_packages[key]['Version'] in elem)
-                        #self.log.append('Correct: ' + key + ' ' + pm4_packages[key]['Version'] + '; ')
-                        #self.log[-1] += 'Correct: ' + key + ' ' + pm4_packages[key]['Version'] + '; '
-                    except:
-                        #self.log.append('Wrong: ' + key + ' version. ')
-                        #self.log[-1] += 'Wrong: ' + key + ' version. '
-                        self.log[-1] += 'INVALID: ' + elem + ' - Expected: ' + pm4_packages[key]['Version'] + '; '
-                        fail_flag = 1
+                        version = re.search(r'(?:Version: )([\d].+)', elem).group(1)
+                        #self.log[-1] += 'Correct: ' + key + ' ' + pm4_packages[key]['Version'] + ' ---------- '
+                        self.log[-1] += 'Correct: ' + key  + ' Version: ' + pm4_packages[key]['Version'] + ' ---------- '
+                    except AssertionError as e:
+                        version = re.search(r'(?:Version: )([\d].+)', elem).group(1)
+                        #self.log[-1] += 'INVALID: ' + key + ' - Expected: ' + pm4_packages[key]['Version'] + ' ---------- '
+                        self.log[-1] += 'INVALID: ' + key + ' Version Seen: ' + version + ' - Expected: ' + pm4_packages[key]['Version'] + ' ---------- '
+                        self.assertionFailures.append(str(e))
 
                     del pm4_packages[key]
                     break
-        if fail_flag == 1:
-            self.fail()
 
 ''' Main call. Only used in test file.'''
 
