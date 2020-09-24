a3c35a9cf183d44f115d14a49b4f360587a194ba
karlOS123testing <carlos.camacho@processmaker.com>
2020-09-24 17:08:00 -0400

    new update

---
+++ includes/__init__ .py
@@ -0,0 +1,7 @@
+from os import environ
+environ['ENVIRONMENT'] = 'local'
+data = {
+    "server_url":"https://release-testing.processmaker.net",
+    "username":"admin",
+    "password":"admin"
+}
--- includes/page_scripts_builder.py
+++ includes/page_scripts_builder.py
@@ -8,42 +8,64 @@ from selenium.common.exceptions import TimeoutException
 import time
 import string
 import util
+from selenium import webdriver
 
 class PageScriptsBuilder:
     ''' Page object model for category page'''
     # constants    
-    SCRIPT_BUILD_CODE_CSS = "div[class='view-lines'"
+    SCRIPT_BUILD_CODE_CSS = "div[class='view-lines']"
+    SCRIPT_BUILD_XPATH = "//span[contains(text(),'[];')]"
     SCRIPT_BUILD_BUTTON_RUN_CSS = "button[class='btn text-capitalize pl-3 pr-3 btn-secondary btn-sm']"
     SCRIPT_BUILD_OUTPUT_CSS = "div[class='output text-white']"
+    SCRIPT_BUILD_SAVE_CSS = "div.p-0>button"
+
 
     def __init__(self, driver, data):
         ''' Instantiate PageProcesses object. '''
         self.driver = driver
         self.data = data
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 60)
 
     def paths_create_scripts_builder(self):
         ''' Function to get page elements. '''
         self.code = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, self.SCRIPT_BUILD_CODE_CSS)))
         self.button_run = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, self.SCRIPT_BUILD_BUTTON_RUN_CSS)))
         self.output = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, self.SCRIPT_BUILD_OUTPUT_CSS)))
+        self.button_save = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, self.SCRIPT_BUILD_SAVE_CSS)))
         
-
     def create_scripts_builder(self, code):
-        self.paths_create_scripts_builder()
-        self.code.wait.until(EC.visibility_of_element_located((By.XPATH,"//span[contains(.,'type here to search')]")))
-        codeHTML = '''
-                <div style="top:228px;height:19px;" class="view-line">
-                    <span><span class="mtk1">&nbsp;</span>
-                    <span class="mtk6">return ;</span>
-                    <span class="mtk1">&nbsp;"this is my test";</span>
-                    </span>
-                </div>
-                '''
-        codeJS
-        self.execute_script()
+        self.paths_create_scripts_builder()     
+
+        elem = self.wait.until(EC.visibility_of_element_located((By.XPATH, self.SCRIPT_BUILD_XPATH)))
+        self.driver.execute_script("""
+                                    var elem = arguments[0];
+                                    console.log(elem); 
+                                    elem.innerHTML = '&nbsp;'
+                                    elem.parentElement.innerHTML += '<span class="mtk20">"smoke&nbsp;test"</span><span class="mtk1">;</span>'
+        """, elem)
+
+      #3  wa = WebDriverWait(self.driver, 30)
+        self.button_save.click()
+#################
+        title =  self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"input.form-control"))).send_keys('1')
+        title2 =  self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"textarea.form-control"))).send_keys('1')
+        save = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"div.modal-body>div.modal-footer>button.btn.btn-secondary")))
+        save.click()
+#################
+
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='alert d-none d-lg-block alertBox alert-dismissible alert-success']")))
         
+        self.button_run.click()
+
+
+        resp='{"output": "smoke test"}'
+
+        #code1 = self.wait.until(EC.visibility_of_element_located((By.XPATH,'//samp[contains(text(),'+resp+')]')))
+        code1 = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"pre.text-white>samp")))
+        
+        print(code1)
+             
         script_code = {
-            'script_builder_code': code
+            'script_builder_code': code1
             }
         return script_code
