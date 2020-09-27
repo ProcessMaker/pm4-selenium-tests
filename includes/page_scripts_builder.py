bd531c5166b76994f3b48982140d23f2aa6355ff
amauricio <ana.mauricio.gutierrez@gmail.com>
2020-09-27 19:40:24 -0400

    Fix time in test_TCP4_784.py

--- includes/page_scripts_builder.py
+++ includes/page_scripts_builder.py
@@ -57,7 +57,7 @@ class PageScriptsBuilder:
 
     def run_script_builder(self):
         self.button_run.click()
-        wait = WebDriverWait(self.driver, 400)
+        wait = WebDriverWait(self.driver, 100)
         wait.until(EC.visibility_of_element_located((By.XPATH, PageScriptsBuilder.FINISH_LOADING_SCRIPT_XPATH)))
 
     def put_script_code(self, code):
