b681989c2572c588a1d3fe6c6c217480a0d7b8f0
amauricio <ana.mauricio.gutierrez@gmail.com>
2020-09-29 09:32:22 -0400

    Fix time in test_TCP4_784.py

--- includes/page_scripts_builder.py
+++ includes/page_scripts_builder.py
@@ -28,7 +28,7 @@ class PageScriptsBuilder:
         ''' Instantiate PageProcesses object. '''
         self.driver = driver
         self.data = data
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 30)
 
     def paths_create_scripts_builder(self):
         ''' Function to get page elements. '''
@@ -57,8 +57,8 @@ class PageScriptsBuilder:
 
     def run_script_builder(self):
         self.button_run.click()
-        wait = WebDriverWait(self.driver, 40)
-        wait.until(EC.visibility_of_element_located((By.XPATH, PageScriptsBuilder.FINISH_LOADING_SCRIPT_XPATH)))
+        # wait = WebDriverWait(self.driver, 30)
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, PageScriptsBuilder.FINISH_LOADING_SCRIPT_XPATH)))
 
     def put_script_code(self, code):
         textarea =self.wait.until(EC.visibility_of_element_located(
