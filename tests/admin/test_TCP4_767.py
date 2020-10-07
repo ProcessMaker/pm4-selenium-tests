439b1b2135577cf109e54e7e69d102da0dd93d0d
amauricio <ana.mauricio.gutierrez@gmail.com>
2020-10-07 11:58:26 -0400

    Add route relatives

---
+++ tests/admin/test_TCP4_767.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python3
+
+# Check if using local environment
+from os import getenv
+import os.path
+if getenv('ENVIRONMENT') == 'local':
+    import sys
+    from sys import path
+
+    path.append('../../includes')
+    from __init__ import data
+
+from test_parent import BaseTest
+import util
+import unittest
+
+
+class TCP4_767(BaseTest):
+    ''' Test to verify import a collections'''
+
+    def test_import_collection(self):
+        file_name = 'id1.png'
+        print(os.path.abspath(file_name), file=sys.stderr)
+
+
+
+if __name__ == "__main__":
+    import __main__
+    output = util.run_test(TCP4_767, data, __main__)
